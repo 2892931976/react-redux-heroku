0c49b72c492d5fac001f9320ca45e998a3730aac
lewis617 <897473127@qq.com>
2016-10-16 19:21:18 +0800

    remove environment var && update some package version

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  host: process.env.HOST || 'localhost',
-  port: process.env.PORT || process.env.NODE_ENV === 'production' ? 8080 : 3000,
-  apiHost: process.env.APIHOST || 'localhost',
-  apiPort: process.env.APIPORT || 3030,
+  host: 'localhost',
+  port: process.env.NODE_ENV === 'production' ? 8080 : 3000,
+  apiHost: 'localhost',
+  apiPort: 3030,
   app: {
     title: 'React Redux Book',
     description: 'React Redux Book Example',
